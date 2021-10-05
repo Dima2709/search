6b74e899b7ec70769bcf07d2911aeb1dcbf58f33
Dima2709 <87536237+Dima2709@users.noreply.github.com>
2021-10-05 15:27:59 +0300

    Add files via upload

---
+++ search.py
@@ -0,0 +1,26 @@
+def search (arg, arg1):
+    with open(arg+'.txt', 'r') as d:
+        s = d.read()
+        s = s.split(' ')
+    b = [' ', '  ', ',', '.', '=', '-', '(', ')', '/', '_','—', '!','?','\n', ':','«','»']
+    for i in range(len(s)):
+        for j in s[i]:
+            if j in b:
+                s[i] = s[i].replace(j, "")
+    o = []
+    for i in s:
+        if len (i) > 0:
+            o.append(i.lower())
+    arg1 = arg1.lower()
+    c = []
+    for i in range(len(o)):
+        if o[i] == arg1:
+            c.append(i)
+    if len (c) > 0:
+        print('Всего слов в тесте - ', len(o))
+        for i in c:
+            print(' Слово, которое вы ищете находится на ', i + 1, 'месте')
+    else:
+            print('Слово не найдено, попробуйте еще раз')
+
+search('test','страшная')
